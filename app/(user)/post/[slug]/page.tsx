07223407746b485077f4055aece21a9e3873be27
Hassan <muhammadhassanchannel786@gmail.com>
2024-01-01 04:02:34 +0500

    feat: dynamic content renderer for sanity with custom classes for every tag

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,7 +1,11 @@
-import { client } from "@/sanity/lib/client";
-import { urlForImage } from "@/sanity/lib/image";
-import { groq } from "next-sanity";
 import Image from "next/image";
+
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
+import { urlForImage } from "@/sanity/lib/image";
+
+import { PortableText } from '@portabletext/react'
+import { RichTextComponents } from "@/components/RichTextComponents";
 
 type Props = {
   params: {
@@ -24,8 +28,8 @@ const PostPage = async ({ params: { slug } }: Props) => {
   return (
     <article className="px-10 pb-28">
       <section className="space-y-2 border border-[#F7AB0A] text-white">
-          <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
-        <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
             <Image
               className="object-cover object-center mx-auto"
               src={urlForImage(post.mainImage).url()}
@@ -35,51 +39,51 @@ const PostPage = async ({ params: { slug } }: Props) => {
           </div>
 
           <section className="p-5 bg-[#F7AB0A] w-full">
-
             <div className="flex flex-col md:flex-row justify-between gap-y-5">
-                <div>
-
+              <div>
                 <h1 className="text-4xl font-extrabold">{post.title}</h1>
                 <p>
-                {new Date(post._createdAt).toLocaleDateString("en-US", {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    })}
+                  {new Date(post._createdAt).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
                 </p>
-                </div>
+              </div>
 
-                <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-2">
                 <Image
-              className="rounded-full"
-              src={urlForImage(post.author.image).url()}
-              alt={post.author.name}
-              height={40}
-              width={40}
-            />
+                  className="rounded-full"
+                  src={urlForImage(post.author.image).url()}
+                  alt={post.author.name}
+                  height={40}
+                  width={40}
+                />
                 <div className="w-64">
-                    <h3 className="text-lg font-bold">{post.author.name}</h3>
-                    <div>
-                        TODO: Author Bio
-                    </div>
+                  <h3 className="text-lg font-bold">{post.author.name}</h3>
+                  <div>TODO: Author Bio</div>
                 </div>
-                </div>
-
+              </div>
             </div>
 
             <div>
-                <h2 className="italic pt-10">{post.description}</h2>
-                <div className="flex items-center justify-end mt-auto space-x-2">
-                    {post.categories.map(category => (
-                        <p key={category._id} className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4">
-                            {category.title}
-                        </p>
-                    ))}
-                </div>
+              <h2 className="italic pt-10">{post.description}</h2>
+              <div className="flex items-center justify-end mt-auto space-x-2">
+                {post.categories.map((category) => (
+                  <p
+                    key={category._id}
+                    className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
+                  >
+                    {category.title}
+                  </p>
+                ))}
+              </div>
             </div>
           </section>
         </div>
       </section>
+
+      <PortableText value={post.body} components={RichTextComponents} />
     </article>
   );
 };
